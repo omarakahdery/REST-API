e366ddadb6d50cc703435be5c2804a5bccaa9deb
omar alkahdery <omar_kadre@yahoo.com>
2021-10-02 01:07:14 -0700

    ADD: Get finish

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+app.set("view engine", "ejs");
+
+// to pars req
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// to store public file
+app.use(express.static("public"));
+
+//to connect to the db
+mongoose.connect("mongodb://localhost:27017/wikidb", { useNewUrlParser: true });
+
+//to Create schema
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+//to Create the modlle
+const Article = mongoose.model("Article", articleSchema);
+
+//TODO/////////////////////////////
+app.route("/articles").get(function (req, res) {
+  Article.find(function (err, foundArticle) {
+    if (!err) {
+      res.send(foundArticle);
+    } else {
+      res.send(`err${err}`);
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
