e5e13554e950882ae6797424b820d425da4a23f8
omar alkahdery <omar_kadre@yahoo.com>
2021-10-02 02:45:36 -0700

    ADD:delet finish

--- app.js
+++ app.js
@@ -50,8 +50,16 @@ app
         res.send("a new article successfully added ");
       }
     });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("All articles successfully deleted ");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .app.listen(3000, function () {
+    console.log("Server started on port 3000");
   });
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
