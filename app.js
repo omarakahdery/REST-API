cd239de1abcdd51d527832fadaeb9ec1c7e26d63
omar alkahdery <omar_kadre@yahoo.com>
2021-10-02 01:32:03 -0700

    ADD:post finish

--- app.js
+++ app.js
@@ -29,15 +29,28 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 //TODO/////////////////////////////
-app.route("/articles").get(function (req, res) {
-  Article.find(function (err, foundArticle) {
-    if (!err) {
-      res.send(foundArticle);
-    } else {
-      res.send(`err${err}`);
-    }
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticle) {
+      if (!err) {
+        res.send(foundArticle);
+      } else {
+        res.send(`err${err}`);
+      }
+    });
+  })
+  .post(function (req, res) {
+    newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("a new article successfully added ");
+      }
+    });
   });
-});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
